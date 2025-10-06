efbec3893179087ae0488738d387c78e0cb47c0d
superrhoca <134966255+superrhoca@users.noreply.github.com>
2025-10-06 21:31:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+async function loadQuestions() {
+  const response = await fetch("sorular/toplama.json");
+  const questions = await response.json();
+  const randomQs = questions.sort(() => 0.5 - Math.random()).slice(0, 10);
+  const leftCol = document.getElementById("left-col");
+  const rightCol = document.getElementById("right-col");
+  randomQs.forEach((q, i) => {
+    let div = document.createElement("div");
+    div.className = "question";
+    div.innerHTML = `<b>${i+1})</b> ${q}<br><span class="answer"></span>`;
+    if (i < 5) { leftCol.appendChild(div); } else { rightCol.appendChild(div); }
+  });
+}
+loadQuestions();
